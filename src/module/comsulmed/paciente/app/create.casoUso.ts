7f1f9c3eaad162439bfe00bd81413a9901b951d2
Cristhian Guedes <cristhianjosue03@outlook.com>
2025-05-13 00:12:29 -0500

    Update create.casoUso.ts

---
+++ src/module/comsulmed/paciente/app/create.casoUso.ts
@@ -0,0 +1,24 @@
+import { IRepositorioPaciente } from "../domain/interface/repository/repositoryPaciente.interface";
+import { codigoAndEdadNegativo, fechaNoFutura, nombreApellidoAndCedulaValidator } from "../domain/validation/validation";
+
+export class CasoUsoCreatePaciente{
+    constructor(private repositorio:IRepositorioPaciente){}
+
+    async run(codigo:number,cedula:string,nombres:string,fechaNacimiento:string,
+        apellidos:string,edad:number,genero:string,telefono:string,
+        tipoSangre:string,direccion:string,correo:string,
+        ocupacion:string
+    ):Promise<void>{
+        const paciente={
+            codigo:codigoAndEdadNegativo(codigo),
+            cedula:nombreApellidoAndCedulaValidator(cedula),
+            nombres:nombreApellidoAndCedulaValidator(nombres),
+            fechaNacimiento:fechaNoFutura(fechaNacimiento),
+            apellidos:nombreApellidoAndCedulaValidator(apellidos),
+            edad:codigoAndEdadNegativo(edad),
+            genero,telefono,tipoSangre,direccion,correo,ocupacion
+        }
+
+        return this.repositorio.create(paciente)
+    }
+}
