7cda8b0756909428c1daea60274385e9fe95c384
Cristhian Guedes <cristhianjosue03@outlook.com>
2025-05-13 00:12:31 -0500

    Update paciente.validation.ts

--- src/module/comsulmed/paciente/domain/validation/util/paciente.validation.ts
+++ src/module/comsulmed/paciente/domain/validation/util/paciente.validation.ts
@@ -2,3 +2,5 @@ export class PacienteNotFound extends Error{}
 export class PacienteIdNegative extends Error{}
 export class PacienteUnnamed extends Error{}
 export class PacienteAgeNegative extends Error{}
+export class PacienteDocument extends Error{}
+export class PacienteDate extends Error{}
