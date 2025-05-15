87b47d1cb3bd2a563f84e1689260da8ae477cebf
Cristhian Guedes <cristhianjosue03@outlook.com>
2025-05-14 22:38:49 -0500

    Update paciente.interface.ts

--- src/module/comsulmed/paciente/domain/interface/paciente.interface.ts
+++ src/module/comsulmed/paciente/domain/interface/paciente.interface.ts
@@ -2,5 +2,13 @@ export interface Paciente{
     codigo?:number
     cedula:string
     nombres:string
+    apellidos:string
+    edad:number
     fechaNacimiento:string
+    genero:string
+    telefono:string
+    tipoSangre:string
+    direccion?:string
+    correo:string
+    ocupacion?:string
 }
