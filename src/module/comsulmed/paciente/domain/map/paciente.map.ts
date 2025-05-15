0828b67ccc9bc7ff350fc2a51c076afcae08ad4b
Cristhian Guedes <cristhianjosue03@outlook.com>
2025-05-14 22:46:38 -0500

    Update paciente.map.ts

---
+++ src/module/comsulmed/paciente/domain/map/paciente.map.ts
@@ -0,0 +1,18 @@
+import { Paciente } from "../interface/paciente.interface";
+
+function mapToDomain(objeto:any):Paciente {
+    return{
+        codigo:objeto.codigo,
+        cedula:objeto.cedula,
+        nombres:objeto.nombres,
+        apellidos:objeto.apellidos,
+        edad:objeto.edad,
+        fechaNacimiento:objeto.fechaNacimiento,
+        genero:objeto.genero,
+        telefono:objeto.telefono,
+        tipoSangre:objeto.tipoSangre,
+        direccion:objeto.direccion,
+        correo:objeto.correo,
+        ocupacion:objeto.ocupacion
+    }
+}
