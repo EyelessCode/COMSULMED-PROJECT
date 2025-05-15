329f785b2e0799ee4969663c71b73153d2b9f885
Cristhian Guedes <cristhianjosue03@outlook.com>
2025-05-14 20:28:57 -0500

    Update repositoryPaciente.interface.ts

--- src/module/comsulmed/paciente/domain/interface/repository/repositoryPaciente.interface.ts
+++ src/module/comsulmed/paciente/domain/interface/repository/repositoryPaciente.interface.ts
@@ -1,9 +1,18 @@
 import { paciente } from "@prisma/client"
+import { Paciente } from "../paciente.interface"
 
 export interface IRepositorioPaciente{
     getAll():Promise<paciente[]>
     getOneById(id:number):Promise<paciente|null>
     create(student:paciente):Promise<void>
     edit(student:paciente):Promise<void>
+    delete(id:number):Promise<void>
+}
+
+export interface IRepositorioTestPaciente{
+    getAll():Promise<Paciente[]>
+    getOneById(id:number):Promise<Paciente|null>
+    create(student:Paciente):Promise<void>
+    edit(student:Paciente):Promise<void>
     delete(id:number):Promise<void>
 }
