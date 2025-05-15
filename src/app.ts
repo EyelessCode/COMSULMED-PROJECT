77f396d14c383910414405edd9d2ad45bdc89b22
Cristhian Guedes <cristhianjosue03@outlook.com>
2025-05-14 21:53:58 -0500

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 import cors from 'cors';
 import "dotenv/config"
 
@@ -7,5 +7,9 @@ const PORT=process.env.PORT||4000
 
 app.use(express.json())
 app.use(cors())
+
+app.get("/test",(req:Request,res:Response)=>{
+    res.send("HOLA MUNDO!")
+})
 
 app.listen(PORT,()=>console.log(`Escuchando http://localhost:${PORT}`))
