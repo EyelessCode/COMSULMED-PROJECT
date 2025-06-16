8ed310735bfd0774df267d05a598314b2731b219
Cristhian Guedes <cristhianjosue03@outlook.com>
2025-06-16 09:27:08 -0500

    Update getAll.casoUso.ts

--- src/module/comsulmed/paciente/app/getAll.casoUso.ts
+++ src/module/comsulmed/paciente/app/getAll.casoUso.ts
@@ -1,10 +1,12 @@
 import { Paciente } from "../domain/interface/paciente.interface";
 import { IRepositorioPaciente } from "../domain/interface/repository/repositoryPaciente.interface";
+import { mapToDomain } from "../domain/map/paciente.map";
 
 export class CasoUsogetAllPaciente{
     constructor(private repositorio:IRepositorioPaciente){}
     
     async run():Promise<Paciente[]>{
-        return this.repositorio.getAll()
+        const pacientes=await this.repositorio.getAll()
+        return pacientes.map(mapToDomain)
     }
 }
